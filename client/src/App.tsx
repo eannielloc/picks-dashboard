d8417900ac0167ce865ffb73b17cf45be33c4409
Chris Eanniello <chriseanniello@Chriss-MacBook-Pro-3.local>
2026-02-13 21:14:48 -0700

    fix: remove double BrowserRouter, update today's picks section with Telegram link

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import History from './pages/History';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/history" element={<History />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/history" element={<History />} />
+    </Routes>
   );
 }
